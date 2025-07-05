d1f2f642dfd7558b7eb9cbecd3b952cdc81b7ed5
Ganesh Padhayay <ganeshraj020794@gmail.com>
2025-07-05 19:59:05 +0530

    added square of sorted array problem with two solution - two pointer with On and simple with Onlong

---
+++ src/array/twopointers/SquareOfSortedArray.java
@@ -0,0 +1,74 @@
+package array.twopointers;
+
+import java.util.Arrays;
+
+/**
+ * Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [-4,-1,0,3,10]
+ * Output: [0,1,9,16,100]
+ * Explanation: After squaring, the array becomes [16,1,0,9,100].
+ * After sorting, it becomes [0,1,9,16,100].
+ * Example 2:
+ *
+ * Input: nums = [-7,-3,2,3,11]
+ * Output: [4,9,9,49,121]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums is sorted in non-decreasing order.
+ */
+public class SquareOfSortedArray {
+
+    public static void main(String[] args) {
+        SquareOfSortedArray solution = new SquareOfSortedArray();
+        // Example 1
+        int[] nums1 = {-4, -1, 0, 3, 10};
+        int[] result1 = solution.sortedSquares(nums1);
+        System.out.println("Result for nums1: " + java.util.Arrays.toString(result1));
+
+        // Example 2
+        int[] nums2 = {-7, -3, 2, 3, 11};
+        int[] result2 = solution.sortedSquares(nums2);
+        System.out.println("Result for nums2: " + java.util.Arrays.toString(result2));
+    }
+
+    //this is accepted but very slow
+    /*public int[] sortedSquares(int[] nums) {
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }*/
+
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[index] = leftSquare;
+                left++;
+            } else {
+                result[index] = rightSquare;
+                right--;
+            }
+            index--;
+        }
+
+        return result;
+    }
+}
